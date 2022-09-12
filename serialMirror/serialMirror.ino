3c9bf9eb9a4bfb6c87889a376e3ef7e5023f682e
akdunn <akdunn@calpoly.edu>
2022-09-12 15:41:56 -0700

    Wrote code to use esp32 as USB to UART bridge

---
+++ serialMirror/serialMirror.ino
@@ -0,0 +1,49 @@
+// Serial Mirror
+// 09/12/22
+
+// Pins
+#define RX1 16
+#define TX1 17
+
+// Task Intervals
+#define TX_INTERVAL 0
+#define RX_INTERVAL 0
+
+long txTimer;
+long rxTimer;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, RX1, TX1);
+}
+
+void loop()
+{
+  taskTX();
+
+  taskRX();
+}
+
+
+void taskTX()
+{
+  if ((millis() - txTimer) > TX_INTERVAL)
+  {
+    while (Serial.available())
+    {
+      Serial1.write(Serial.read());
+    }
+  }
+}
+
+void taskRX()
+{
+  if ((millis() - rxTimer) > RX_INTERVAL)
+  {
+    while (Serial1.available())
+    {
+      Serial.write(Serial1.read());
+    }
+  }
+}
